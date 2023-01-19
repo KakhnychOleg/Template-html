fc3b66570afa9d47a56141d54b049b6f462ba612
Oleg <kakhnych.oleg@gmail.com>
2023-01-19 16:50:25 +0300

    init code

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  // Menu
+  $('.menu__burger').click(function(event) {
+    $('.menu__burger, .menu').toggleClass('active');
+    $('body').toggleClass('lock');
+  })
+  
+  // Slider
+  $('.slider__list').slick({
+    dots: true,
+    infinite: true,
+    // nextArrow: '.slick-next-slide',
+    // prevArrow: '.slick-prev-slide',
+    // appendDots: '.slider__dots',
+    autoplay: true,
+    speed: 500,
+    pauseOnHover: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+
+  // Accordion
+  $('.accordion__title').click(function(event) {
+    if($('.accordion').hasClass('one')){
+      $('.accordion__title').not($(this)).removeClass('active');
+      $('.accordion__text').not($(this).next()).slideUp(300);
+    }
+    $(this).toggleClass('active').next().slideToggle(300)
+  });
+});
